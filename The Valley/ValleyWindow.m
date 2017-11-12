519058193c8185ce7d3a676ba579eb86e10fd44f
Tony Smith <tony@electricimp.com>
2017-11-12 14:04:33 +0000

    Build 3: Add key/main window overrides

--- The Valley/ValleyWindow.m
+++ The Valley/ValleyWindow.m
@@ -21,47 +21,63 @@
 }
 
 
-
 - (void)sendEvent:(NSEvent *)event
 {
     // Intercept key presses during combat, if 'enableKeys' is false
 
-    if (!enableKeys && (event.type == NSEventTypeKeyUp || event.type == NSEventTypeKeyDown))
+    if (event.type == NSEventTypeKeyUp || event.type == NSEventTypeKeyDown)
     {
-        NSString *c = event.characters;
-        BOOL value = [c compare:@"B"] || [c compare:@"L"] || [c compare:@"H"] || [c compare:@"S"] ||
-        [c compare:@"1"] || [c compare:@"2"] || [c compare:@"3"] || [c compare:@"4"] || [c compare:@"5"] ||
-        [c compare:@"6"] || [c compare:@"7"] || [c compare:@"8"] || [c compare:@"9"];
-
-        if (value)
+        if (!enableKeys)
         {
-            // B, H or L pressed at a time when we don't want to record those presses
-            // So generate an alternative event to replace the incoming one
+            NSString *c = event.characters;
+            BOOL value = [c compare:@"B"] || [c compare:@"L"] || [c compare:@"H"] || [c compare:@"S"] ||
+            [c compare:@"1"] || [c compare:@"2"] || [c compare:@"3"] || [c compare:@"4"] || [c compare:@"5"] ||
+            [c compare:@"6"] || [c compare:@"7"] || [c compare:@"8"] || [c compare:@"9"];
 
-            NSEvent *newEvent = [NSEvent keyEventWithType:event.type
-                                                 location:event.locationInWindow
-                                            modifierFlags:event.modifierFlags
-                                                timestamp:event.timestamp
-                                             windowNumber:event.windowNumber
-                                                  context:nil
-                                               characters:@" "
-                              charactersIgnoringModifiers:@" "
-                                                isARepeat:NO
-                                                  keyCode:event.keyCode];
+            if (value)
+            {
+                // B, H or L pressed at a time when we don't want to record those presses
+                // So generate an alternative event to replace the incoming one
 
-            // And pass it along the chain
+                NSEvent *newEvent = [NSEvent keyEventWithType:event.type
+                                                     location:event.locationInWindow
+                                                modifierFlags:event.modifierFlags
+                                                    timestamp:event.timestamp
+                                                 windowNumber:event.windowNumber
+                                                      context:nil
+                                                   characters:@" "
+                                  charactersIgnoringModifiers:@" "
+                                                    isARepeat:NO
+                                                      keyCode:event.keyCode];
 
-            [super sendEvent:newEvent];
+                // And pass it along the chain
 
-            return;
+                [super sendEvent:newEvent];
+
+                return;
+            }
         }
+
+        // Block all key repeat events
+
+        if ((event.type == NSEventTypeKeyUp || event.type == NSEventTypeKeyDown) && event.isARepeat) return;
     }
 
-    // Block all key repeat events
-
-    if ((event.type == NSEventTypeKeyUp || event.type == NSEventTypeKeyDown) && event.isARepeat) return;
-
     [super sendEvent:event];
+}
+
+
+
+- (BOOL)canBecomeKeyWindow
+{
+    return YES;
+}
+
+
+
+- (BOOL)canBecomeMainWindow
+{
+    return YES;
 }
 
 
