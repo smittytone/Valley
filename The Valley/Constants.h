06ba001cdd809b5aeadf30676621d5612951bcca
Tony Smith <tony@electricimp.com>
2017-11-13 19:50:18 +0000

    Remove unwanted constant

--- The Valley/Constants.h
+++ The Valley/Constants.h
@@ -9,7 +9,6 @@
 // Valley Graphics
 
 #define    kGraphicSpace           32
-#define    kGraphicSpaceAlt        32
 #define    kGraphicCharacter       81
 #define    kGraphicBorder          214
 #define    kGraphicSafeCastle      209
