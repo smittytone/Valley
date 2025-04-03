4dfde1e79b62728e32192c3e051a1a3f92058f31
Tony Smith (smittytone) <tony@smittytone.net>
2025-04-03 09:38:40 +0100

    Update dates

--- The Valley/Character.h
+++ The Valley/Character.h
@@ -1,6 +1,6 @@
 
 
-// macOS software © Tony Smith 2012-20, based on work
+// macOS software © Tony Smith 2012-25, based on work
 // by Henry Budget, Peter Freebrey, Peter Green and Ron Harris.
 // Originally published by Computing Today
 // magazine in its April 1982 issue.
